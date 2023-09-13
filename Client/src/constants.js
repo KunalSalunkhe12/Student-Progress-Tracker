a661214a3cf4ae0c5c62878f8936546454232ef5
Kunal Rajesh Salunke <salunkhekunal594@gmail.com>
2023-09-14 00:54:46 +0530

    Update constants.js

--- Client/src/constants.js
+++ Client/src/constants.js
@@ -1,2 +1,2 @@
-export const API_ENDPOINT = "http://localhost:5000/"
-// export const API_ENDPOINT = "https://student-progress-tracker-main-server.onrender.com/"
+//export const API_ENDPOINT = "http://localhost:5000/"
+export const API_ENDPOINT = "https://student-progress-tracker-main-server.onrender.com/"
